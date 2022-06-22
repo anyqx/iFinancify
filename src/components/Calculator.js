f86409486a9bd4005e04b06fd1af5e0f79292e4c
Annie Xu <annie.xu@stessa.com>
2022-06-21 22:23:26 -0700

    Create app, add MUI yarn package

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import './Calculator.scss';
+
+const Calculator = () => {
+  return (
+    <div className="calculator">
+      <header className="calculator-header">
+        <h1>Mortgage Calculator test</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Calculator;
